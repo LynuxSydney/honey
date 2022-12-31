d70a3125f26a6c6f2d3d966e9f5f23f958ff4421
Beri Contraster <74823071+LynuxSydney@users.noreply.github.com>
2022-12-31 15:27:43 +0500

    Update raw_id.py

--- raw_id.py
+++ raw_id.py
@@ -1,7 +1,7 @@
 #fname = input("Enter file name: ")
-raw_file = "C:\pwsh.txt"
+raw_file = "C:\Windows\System32\lsass.txt"
 id = []
-f = open("C:\id_lsass.txt",'w')
+f = open("C:\Windows\System32\id_lsass.txt",'w')
 f.close()
 with open(raw_file, 'r') as f:
     for line in f:
@@ -10,5 +10,5 @@ with open(raw_file, 'r') as f:
             for letter in i:
                 if(letter.isdigit()):
                     data = letter
-                    f = open("C:\id_lsass.txt",'a')
+                    f = open("C:\Windows\System32\id_lsass.txt",'a')
                     f.write(data)
